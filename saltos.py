4f85b9053cf612d7199d4d37d56486e62a603aa3
GuilhermeMVasquez <guilhermemalgarizi@gmail.com>
2024-09-28 09:36:26 -0300

    memoization

--- saltos.py
+++ saltos.py
@@ -6,14 +6,19 @@ def recur(n):
     if str[n] == '0': return 0
     return recur(n-1) + recur(n-2) + (1 if n-3 == 0 else 0) + ((recur(n-4) + recur(n-5)) if n-3 > 0 and str[n-3] == '1' else 0)
 
-# def recurMem(str):
-
+mem = {}
+def recurMem(n):
+    if n in mem: return mem[n]
+    if n < 0: return 0
+    if n == 0: return 1
+    if str[n] == '0': return 0
+    mem[n] = recur(n-1) + recur(n-2) + (1 if n-3 == 0 else 0) + ((recur(n-4) + recur(n-5)) if n-3 > 0 and str[n-3] == '1' else 0)
+    return mem[n]
 
 # def noRecur(str):
 
 
-
 str = argv[1]
 print("Recursao simples:", recur(len(str)-1))
-# print("Recursao memorizada:", recurMem(str))
+print("Recursao memorizada:", recurMem(len(str)-1))
 # print("Sem recursao:", noRecur(str))
