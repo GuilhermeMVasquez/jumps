0f42fa4aa89fc8b7d3225dc713d7554e7d0f261a
GuilhermeMVasquez <guilhermemalgarizi@gmail.com>
2024-09-28 18:00:00 -0300

    recur limit

--- saltos.py
+++ saltos.py
@@ -1,4 +1,6 @@
-from sys import argv
+import sys
+
+sys.setrecursionlimit(10**9)
 
 def recur(n):
     if n < 0: return 0
@@ -30,7 +32,7 @@ def noRecur(n):
                 array[i] += array[i-5] if i-5 >= 0 else 0
     return array[n]
 
-str = argv[1]
+str = sys.argv[1]
 print("Recursao simples:    existem", recur(len(str)-1), "maneiras")
 print("Recursao memorizada: existem", recurMem(len(str)-1), "maneiras")
 print("Sem recursao:        existem", noRecur(len(str)-1), "maneiras")
